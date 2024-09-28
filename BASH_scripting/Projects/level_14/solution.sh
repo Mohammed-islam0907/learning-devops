3c7b381599e329c546eb8cc91dd6248486e114a4
ikram <ikram30002@gmail.com>
2024-09-28 20:08:04 +0100

    l 14

---
+++ BASH_scripting/Projects/level_14/solution.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "What would to like to do?"
+
+echo "1. Check disk space"
+echo "2. Show system uptime"
+echo "3. List users"
+
+echo "Enter a number"
+
+read number 
+
+case $number in
+
+    1) df -h;;
+    2) uptime;;
+    3) users;;
+    *) echo "Didn't select a valid option"
+esac
