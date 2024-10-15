550e1a0a6588f3f5530e70100f93ded0f4067ed4
ikram <ikram30002@gmail.com>
2024-10-15 02:08:27 +0100

    update app.py

--- Docker/hello_flask/app.py
+++ Docker/hello_flask/app.py
@@ -1,12 +1,23 @@
-
+# app.py
 
 from flask import Flask
+import MySQLdb
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    # Connect to the MySQL database
+    db = MySQLdb.connect(
+        host="mydb",    # Hostname of the MySQL container
+        user="root",    # Username to connect to MySQL
+        passwd="my-secret-pw",  # Password for the MySQL user
+        db="mysql"      # Name of the database to connect to
+    )
+    cur = db.cursor()
+    cur.execute("SELECT VERSION()")
+    version = cur.fetchone()
+    return f'Hello, World! MySQL version: {version[0]}'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5002)
