baf6129faee22d5561a6417b912eaf42f6705c12
ikram <ikram30002@gmail.com>
2024-10-20 00:10:14 +0100

    update app.py

--- Docker/hello_flask/Project/app.py
+++ Docker/hello_flask/Project/app.py
@@ -5,23 +5,24 @@ import redis
 
 app = Flask(__name__)
 
-
+# Initialize Redis connection
 r = redis.Redis(
-        host='redis', 
-        port=6379, 
-        decode_responses=True,
-        db=0
+    host='redis', 
+    port=6379, 
+    decode_responses=True,
+    db=0
 ) 
 
 @app.route('/')
 def greetings():
+    # Render template without additional content
     return render_template('layout.html')
 
 @app.route('/count')
 def count():
-    count = r.incr('visitors')
-    return render_template('layout.html', content=f'This page has been visited {count} times.')
-
+    # Increment visitor count and pass it to the template
+    visit_count = r.incr('visitors')
+    return render_template('layout.html', content=f'This page has been visited {visit_count} times.')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
