8b5bfc86960cef8d27d355e271e91919126a0b6b
ikram <ikram30002@gmail.com>
2024-11-23 10:25:20 +0000

    update ec2.tf

---
+++ Terraform/lab/ec2.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "this" {
+  ami                     = "ami-0e8d228ad90af673b"
+  instance_type           = "t2.micro"
+}
+
+resource "aws_instance" "import" {
+  ami                     = "ami-0e8d228ad90af673b"
+  instance_type           = "t2.micro"
+
+     tags = {
+    Name = "terraform-import"
+  }
+}
+
